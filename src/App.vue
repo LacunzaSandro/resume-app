06f2a030ddf66a0570c6d9a31ed6f712bd0630bc
Sandro Lacunza <sandrolacunza@gmail.com>
2021-11-14 15:14:43 -0300

    change scroll's color

--- src/App.vue
+++ src/App.vue
@@ -33,4 +33,29 @@
 
 <style lang="scss">
   @import '@/scss/_base.scss';
+  ::-webkit-scrollbar {
+    width: 8px;
+    background-color: rgb(172, 7, 215);
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: rgb(172, 7, 215);
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background-color: rgb(172, 7, 215);
+    text-decoration: none;
+}
+
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 4px rgba(0,0,0,0.6);
+    background-color: gray;
+}
+
+::-webkit-scrollbar-track:hover {
+    background-color: gray;
+}
+.no-decoration:hover {
+  background-color: gray;;
+}
 </style>
