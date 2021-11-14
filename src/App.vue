c59f1c73bc01c23269334aae0373a3b7eb140887
Sandro Lacunza <sandrolacunza@gmail.com>
2021-11-14 09:59:01 -0300

    se realiza el banner principal

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
     id="app"
     class="container-fluid">
     <Header />
+    <Banner />
   </div>
 </template>
 
 <script>
   import Header from '@/components/Header'
-
+  import Banner from '@/components/Banner.vue'
 
   export default {
     name: "App",
     components: {
       Header,
+      Banner,
     },
   };
 </script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="banner">
+  </div>
+</template>
+
+<style lang="scss">
+  .banner {
+    height: 100vh;
+    width: 100%;
+    background: url('~@/assets/12728.png') center / cover fixed no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+</style>
